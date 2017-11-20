47ffcd55c21ef75ad27ae4558b52f5c9751fcb44
Ryan Neph <neph320@gmail.com>
2017-11-20 14:51:32 -0800

    disable qml caching

--- smallsocc/main.py
+++ smallsocc/main.py
@@ -1,16 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""
-main.py
+"""main.py
 
 control frontend for Sparse Orthogonal Collimator (SOC)
 """
-
 import sys
 import os
 from os.path import dirname
+import signal
+signal.signal(signal.SIGINT, signal.SIG_DFL) # allow ctrl-c kill
 
-# We use QWidget to represent the window, QApplication as the non-vis container for the window
 from OpenGL import GL
 from PyQt5 import QtCore
 from PyQt5.QtGui import QGuiApplication
@@ -18,20 +17,15 @@ from PyQt5.QtQml import QQmlApplicationEngine
 from PyQt5.QtQuick import QQuickItem
 
 from version import VERSION_FULL
-
-# allow ctrl-c kill
-import signal
-signal.signal(signal.SIGINT, signal.SIG_DFL)
-
 from hardware import HWSOC
 import leaflet
 import leafletassembly
 import sequence
 
 # define paths
-parent = os.path.abspath(dirname(dirname(__file__)))
-TEST_FILES = os.path.join(parent, 'test_files')
-TEMP = os.path.join(parent, 'temp')
+PARENT = os.path.abspath(dirname(dirname(__file__)))
+TEST_FILES = os.path.join(PARENT, 'test_files')
+TEMP = os.path.join(PARENT, 'temp')
 
 # TODO: Add proper python logger and integrate with Qt Message Handler
 def qt_message_handler(mode, context, message):
@@ -50,38 +44,42 @@ def qt_message_handler(mode, context, message):
     except: base = str(context.file)
     print('qml- {!s}:L{!s} -{!s}:  {!s}'.format(base, context.line, mode, message))
 
-# TODO: DEBUG
-# load example sequence, otherwise blank
-try:
-    samplelistmodel = sequence.SequenceListModel.fromJson(os.path.join(TEST_FILES, 'test_output.json'))
-except Exception as e:
-    print('FAILED TO READ DEBUG JSON FILE : {!s}'.format(e))
-    # SAMPLE ITEMS FOR DEBUG
-    from sequence import SequenceItem, SequenceItemType
-    sample_sequenceitems = [
-        SequenceItem(rot_couch_deg=5, rot_gantry_deg=0, timecode_ms=1500, datecreatedstr="2016 Oct 31 12:00:00", type='Manual'),
-        SequenceItem(rot_couch_deg=12, rot_gantry_deg=120, timecode_ms=1500, description="descriptive text2", type=SequenceItemType.Auto),
-        SequenceItem(rot_couch_deg=24, rot_gantry_deg=25, timecode_ms=1500, description="descriptive text3"),
-        SequenceItem(rot_couch_deg=0, rot_gantry_deg=45, timecode_ms=1500, description="descriptive text4"),
-    ]
-    samplelistmodel = sequence.SequenceListModel(elements=sample_sequenceitems)
-
 def preExit():
     """ occurs just before Qt application exits (bound to QGuiApplication.aboutToQuit() signal) """
     #  samplelistmodel.writeToJson(os.path.join(TEMP, 'test_write.json'))
     pass
-# TODO: END DEBUG
-
 
 
 ####################################################################################################
 # Start GUI
 if __name__ == '__main__':
+    # TODO: DEBUG
+    # load example sequence, for rapid debugging
+    try:
+        samplelistmodel = sequence.SequenceListModel.fromJson(os.path.join(TEST_FILES, 'test_output.json'))
+    except Exception as e:
+        print('FAILED TO READ DEBUG JSON FILE : {!s}'.format(e))
+        # SAMPLE ITEMS FOR DEBUG
+        from sequence import SequenceItem, SequenceItemType
+        sample_sequenceitems = [
+            SequenceItem(rot_couch_deg=5, rot_gantry_deg=0, timecode_ms=1500, datecreatedstr="2016 Oct 31 12:00:00", type='Manual'),
+            SequenceItem(rot_couch_deg=12, rot_gantry_deg=120, timecode_ms=1500, description="descriptive text2", type=SequenceItemType.Auto),
+            SequenceItem(rot_couch_deg=24, rot_gantry_deg=25, timecode_ms=1500, description="descriptive text3"),
+            SequenceItem(rot_couch_deg=0, rot_gantry_deg=45, timecode_ms=1500, description="descriptive text4"),
+        ]
+        samplelistmodel = sequence.SequenceListModel(elements=sample_sequenceitems)
+    # TODO: END DEBUG
+
     HWSOC(8) # init singleton instance for controlling hardware
 
+    # integrate qml logging with python logging
     QtCore.qInstallMessageHandler(qt_message_handler)
+    # prevent qml caching
+    os.environ['QML_DISABLE_DISK_CACHE'] = '1'
 
+    # set QML visual style
     app = QGuiApplication(sys.argv + ['-style', 'default'])
+    # register pre-exit hook
     app.aboutToQuit.connect(preExit) # perform cleanup
 
     ### the order here matters greatly - must init context properties before loading main.qml
@@ -97,11 +95,10 @@ if __name__ == '__main__':
     engine.load(QtCore.QUrl(os.path.join(dirname(__file__), 'main.qml')))
     rootObject = engine.rootObjects()[0]
 
-    ## connect signals to slots
+    ## connect signals to slots - unnecessary, example of grabbing qml objects from py-code
     # listview buttons
     #  btns = rootObject.findChildren(QQuickItem, "list_buttons", QtCore.Qt.FindChildrenRecursively)[0]
     #  btns.findChild(QQuickItem, 'btn_moveup').clicked.connect(lambda: print('moveup clicked'))
-
 
     # block until window is closed - event handler
     sys.exit(app.exec_())
